f50725dad55f9477be3455e0a1a872a31581a30d
skosanalindiwe2005-debug <skosanalindiwe2005@gmail.com>
2025-11-27 14:38:43 +0200

    Added basic JavaScript and added Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter(".poem", {
+    strings: "Ngisahamba kuwo ohlabayo umhlabathi.",
+    autoStart: true,
+    delay: 40,
+    cursor: "",
+  });
+}
+
+let formElement = document.querySelector("#search-form");
+formElement.addEventListener("submit", generatePoem);
