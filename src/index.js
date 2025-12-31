e59bbfbb5fe936ffab85fb5191554e4e97463ea0
skosanalindiwe2005-debug <skosanalindiwe2005@gmail.com>
2025-12-31 22:51:20 +0200

    Improved the User Experience

--- src/index.js
+++ src/index.js
@@ -10,16 +10,18 @@ function displayPoem(response) {
 function generatePoem(event) {
   event.preventDefault();
 
-  let poem = document.querySelector(".poem");
-  poem.innerHTML = "Generating a poem...";
-
   let searchInputElement = document.querySelector("#search-form-input");
+  let poem = document.querySelector(".poem");
+  poem.innerHTML = `<span class="generating"> ⏳ Generating a Zulu poem about ${searchInputElement.value}</span>`;
 
   let apiKey = "a15t332fa59f4b3b71db27ddcdod075f";
   let prompt = `User instructions are: Generate a Zulu poem about ${searchInputElement.value}`;
   let context =
     "You are an AI model with lots of knowledge about poems and you love to write short romantic poems. Your mission is to always generate a four line poem divide by <br /> elements in basic HTML. Make sure to follow the user instructions. Sign with 'Lindiwe's AI' in a <strong> and <em> elements at the end of the poem. Do not include that it is in HTML.Please behave!";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.style.display = "block";
 
   console.log("Generating poem");
   console.log(`Prompt: ${prompt}`);
